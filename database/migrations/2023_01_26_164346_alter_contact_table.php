3c18b10f1586d45613f36ac0d3567cd9e9393e05
joaocoelho <joao.coelho@blueservice.com.br>
2023-01-26 13:45:14 -0300

    Remove restriction

---
+++ database/migrations/2023_01_26_164346_alter_contact_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->dropUnique(['contact', 'email']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
